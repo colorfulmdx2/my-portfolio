71bf54f9eabbea3a185267939dd6ffaa8fb046ac
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-05-21 19:29:32 +0300

    Updates

---
+++ precache-manifest.383b24645b090b59ece8d2da7fd755c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984dad5dc0adaa8603d1e7b890831dbf",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "806b3b73e8424516dc1c",
+    "url": "/my-portfolio/static/css/main.2372bad2.chunk.css"
+  },
+  {
+    "revision": "4175ee09b5418cb3576f",
+    "url": "/my-portfolio/static/js/2.8f9691e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.8f9691e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806b3b73e8424516dc1c",
+    "url": "/my-portfolio/static/js/main.6e30a8d2.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  }
+]);
