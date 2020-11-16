eb3d04085fec428cc8c22bb1408239099771f233
colorfulmdx2 <colorfulmdx2@gmail.com>
2020-11-16 18:26:59 +0300

    Updates

--- precache-manifest.a664746c5f6899196654e8d169cf09db.js
+++ precache-manifest.a664746c5f6899196654e8d169cf09db.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2472ea726191f6c2d4684afa85681444",
+    "revision": "45ad4f5a3caa5005ff57a69d6e43e7b5",
     "url": "/my-portfolio/index.html"
   },
   {
-    "revision": "ef4d0ceb2031fc58a12b",
-    "url": "/my-portfolio/static/css/main.e6af6fde.chunk.css"
+    "revision": "5bb974694e0f44f5ad20",
+    "url": "/my-portfolio/static/css/main.7573df01.chunk.css"
   },
   {
-    "revision": "0b3a2884be2c206e9495",
-    "url": "/my-portfolio/static/js/2.221c70b6.chunk.js"
+    "revision": "f79771c9ad0c53b6b499",
+    "url": "/my-portfolio/static/js/2.d5f1a377.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/my-portfolio/static/js/2.221c70b6.chunk.js.LICENSE.txt"
+    "url": "/my-portfolio/static/js/2.d5f1a377.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef4d0ceb2031fc58a12b",
-    "url": "/my-portfolio/static/js/main.f894ac07.chunk.js"
+    "revision": "5bb974694e0f44f5ad20",
+    "url": "/my-portfolio/static/js/main.17c71658.chunk.js"
   },
   {
     "revision": "6d7a7337562760299d40",
     "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
   },
   {
-    "revision": "26e8ee43621d2c01f2a3d6d5ddb135b7",
-    "url": "/my-portfolio/static/media/Fuego_de_Refineria.26e8ee43.mp4"
+    "revision": "fbeeab81d81b994cbe0ad8d80ae35552",
+    "url": "/my-portfolio/static/media/blue.fbeeab81.m4v"
+  },
+  {
+    "revision": "8acf7094e9fbf27d5aa8ac8f872d07dd",
+    "url": "/my-portfolio/static/media/boxes.8acf7094.gif"
   },
   {
     "revision": "49f5495c2fae9b6c189e8a6d86832410",
